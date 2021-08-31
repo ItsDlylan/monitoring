9e958e8f2d338356f71fe10122e804f6ed780c19
Dylan Schwindt <dfieldmark69@gmail.com>
2021-08-31 10:15:20 -0700

    added port

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, './public/index.html'))
-})
+const port = process.env.PORT || 4545;
 
-app.listen(4545, ()=> console.log('take us to warp 4545'))
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.listen(port, () => console.log(`take us to warp ${port}`));
