877f310aa7c69b57ae0c073f27ea9ce4cbacf8d9
Dylan Schwindt <dfieldmark69@gmail.com>
2021-08-31 11:05:05 -0700

    added student backend

--- server.js
+++ server.js
@@ -4,11 +4,26 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
+app.use(rollbar.errorHandler());
+
 const port = process.env.PORT || 4545;
+const students = [];
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, './public/index.html'));
 	rollbar.info('html file served succesfully');
+});
+app.post('/api/student', (req, res) => {
+	let { name } = req.body;
+	name = name.trim();
+
+	students.push(name);
+	rollbar.log('student added succesfully', {
+		author: 'Scott',
+		type: 'manual entry',
+	});
+
+	res.status(200).send(students);
 });
 
 app.listen(port, () => console.log(`take us to warp ${port}`));
